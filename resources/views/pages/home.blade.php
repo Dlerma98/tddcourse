42f9dd18a2815c6bfa37a2a6da7265f3e9820049
Dlerma98 <davidglerma1990@gmail.com>
2024-12-17 17:14:26 +0100

    Paso 15 añadir 2 nuevos test al home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,3 +1,15 @@
+@guest
+    <a href="{{route('login')}}">Login</a>
+
+@else
+    <form action="{{route('logout')}}" method="POST">
+
+        @csrf
+        <button type="submit">Log out</button>
+
+    </form>
+
+@endguest
 @foreach($courses as $course)
     <h2>{{ $course->title }}</h2>
     <p>{{ $course->description }}</p>
