162e4fefe9fe760b4f230f9ae88e38a9acb622c3
Dlerma98 <davidglerma1990@gmail.com>
2024-12-18 15:47:30 +0100

    Fin paso 16 añadir test

--- tests/Feature/Pages/PagesResponseTest.php
+++ tests/Feature/Pages/PagesResponseTest.php
@@ -29,3 +29,9 @@ it('gives back successful for dashboard page', function () {
     get(route('pages.dashboard'))
         ->assertOk();
 });
+
+it('does not find Jetstream registration page', function () {
+    //Act & Assert
+    get('register')
+        ->assertNotFound();
+});
