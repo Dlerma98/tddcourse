a54c411d6b4f2e0812bb9060ba43158fd0587727
Dlerma98 <davidglerma1990@gmail.com>
2025-01-08 15:43:53 +0100

    Fin paso 20 mas test y relacion inversa

--- app/Http/Livewire/VideoPlayer.php
+++ app/Http/Livewire/VideoPlayer.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Livewire;
 
+
 use Livewire\Component;
 
 class VideoPlayer extends Component
 {
     public $video;
+    public $courseVideos;
+    public function mount()
+    {
+        $this->courseVideos = $this->video->course->videos;
+    }
+
     public function render()
     {
         return view('livewire.video-player');
