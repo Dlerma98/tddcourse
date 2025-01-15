cc8b31f270fb54712b901cf06846c81248bf5f21
Dlerma98 <davidglerma1990@gmail.com>
2025-01-15 17:11:54 +0100

    Paso 30 a medio incluyendo funciones y mas test

--- resources/views/pages/course-details.blade.php
+++ resources/views/pages/course-details.blade.php
@@ -8,3 +8,10 @@
     @endforeach
 </ul>
 <img src="{{ asset("images/$course->image_name") }}" alt="Image of the course {{ $course->title }}">
+<a href="#!" class="paddle_button" data-product="{{ $course->paddle_product_id }}">Buy Now</a>
+<script src="https://cdn.paddle.com/paddle/paddle.js"></script>
+<script type="text/javascript">
+    @env('local')
+        Paddle.Environment.set('sandbox');
+    @endenv
+</script>
